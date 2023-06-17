d8552e31993400d654c08ac6ba320497b03f8c8b
R3tarb0y <102411483+R3tarb0y@users.noreply.github.com>
2023-06-17 17:46:14 +0700

    Add files via upload

---
+++ Materi_10_2/BusScheduler/app/src/main/java/com/example/busscheduler/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.busscheduler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
